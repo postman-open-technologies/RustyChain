86375b6ad8d1ea3a4d3ff3499d419cb59437c5ca
Mike Ralphson <mike.ralphson@gmail.com>
2023-05-20 02:26:56 +0100

    build: fix some compilation errors
    Signed-off-by: Mike Ralphson <mike.ralphson@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use serde_json::Value;
 use std::env;
 use std::fs;
+
+use serde_json::Value;
+use reqwest::header::{HeaderMap, HeaderValue};
+use eval;
+use rustyline::Editor;
     
+const openai_key: String = env::var("OPENAI_API_KEY").unwrap();
+const bing_api_key: String = env::var("BING_SEARCH_API_KEY").unwrap();
+
 // Use Bing Search API to answer questions
 async fn bing_search(question: &str) -> Option<String> {
-    let api_key = env::var("BING_SEARCH_API_KEY")?;
-    let url = format!("https://api.bing.microsoft.com/a66fa414-3d6b-4925-88e1-21dec2c33a00/bing/v7.0/search?q={}&mkt=en-us&form=WNSG1", question);
-    let resp = reqwest::get(&url).await?;
-    let data: Value = resp.json().await?;
+    let url = format!("https://api.bing.microsoft.com/v7.0/search?q=${question}");
+    let resp = reqwest::get(&url, { "Ocp-Apim-Subscription-Key": HeaderValue = bing_api_key }: HeaderMap).await.unwrap();
+    let data: Value = resp.unwrap().json().await.unwrap();
     data["webPages"]["value"][0]["snippet"].as_str().map(String::from)
 }
 
@@ -17,15 +23,16 @@ struct Tools {
     calculator: (fn(String) -> String, &'static str), 
 }
     
-let tools = Tools {
+const tool:Tools = Tools {
         search: (bing_search, "a search engine. useful for answering questions about current events. input should be a search query."), 
         calculator: (|input| eval::eval(&input).to_string(), 
                      "Useful for getting the result of a mathematical expression. The input to this tool \
                      should be a valid mathematical expression that could be executed by a scientific \
                      calculator.")
+};
 
 // Use GPT-3.5 to complete prompts
-async fn complete_prompt(prompt: String, openai_key: &str) -> String {
+async fn complete_prompt(prompt: String) -> String {
     let url = "https://api.openai.com/v1/completions";
     let body = serde_json::json!({
         "model": "text-davinci-003",
@@ -45,7 +52,7 @@ async fn complete_prompt(prompt: String, openai_key: &str) -> String {
 } 
 
 // Answer a question by constructing prompts for the model 
-async fn answer_question(question: &str, tools: &Tools, openai_key: &str) -> String {
+async fn answer_question(question: &str, tools: &Tools) -> String {
     // Construct the prompt with the question and tool descriptions
     let prompt_template = fs::read_to_string("prompt.txt").unwrap();
     let prompt = prompt_template.replace("${question}", question)
@@ -56,7 +63,7 @@ async fn answer_question(question: &str, tools: &Tools, openai_key: &str) -> Str
     // Iterate until a final answer is found
     let mut prompt = prompt;
     loop {
-        let response = complete_prompt(prompt.clone(), openai_key).await;
+        let response = complete_prompt(prompt.clone().await);
         prompt.push_str(&response);
         
         if let Some(action) = response.split_once("Action: ") {
@@ -71,18 +78,17 @@ async fn answer_question(question: &str, tools: &Tools, openai_key: &str) -> Str
 }
   
 // Merge the chat history with a new question
-async fn merge_history(question: &str, history: &str, openai_key: &str) -> String {
+async fn merge_history(question: &str, history: &str) -> String {
     let prompt_template = fs::read_to_string("merge.txt").unwrap();
     let prompt = prompt_template.replace("${question}", question)
         .replace("${history}", history);
-    complete_prompt(prompt, openai_key).await
+    complete_prompt(prompt.await);
 }
 
 #[tokio::main]
 async fn main() {
-    let openai_key = env::var("OPENAI_API_KEY")?;
     let tools = Tools {
-        search: (google_search, "a search engine. useful for when you need to answer questions about current events. input should be a search query."),
+        search: (bing_search, "a search engine. useful for when you need to answer questions about current events. input should be a search query."),
         calculator: (|input| eval::eval(&input).to_string(), "Useful for getting the result of a math expression. The input to this tool should be a valid mathematical expression that could be executed by a simple calculator.")
     };
     
@@ -92,11 +98,11 @@ async fn main() {
         let question = rl.readline("How can I help? ");
         
         if !history.is_empty() {
-            question = merge_history(&question.unwrap(), &history, &openai_key).await;
+            question = merge_history(&question.unwrap(), &history).await;
         } 
         
-        let answer = answer_question(&question.unwrap(), &tools, &openai_key).await;
+        let answer = answer_question(&question.unwrap(), &tools).await;
         println!("{}", answer);
         history.push_str(&format!("Q:{}\nA:{}\n", question.unwrap(), answer));
-    }
+    };
 }
